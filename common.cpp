7aa1779899510a4e346ee2e9503b4dce8ac0d658
Johan Beimers <35890334+JohanOnPc@users.noreply.github.com>
2025-03-20 01:08:20 +0100

    Added a simple fix to the deviation of the normal distribution for the initializing of the weights

--- common.cpp
+++ common.cpp
@@ -8,7 +8,7 @@ void InitWeights(std::vector<float>& weights, size_t amount, size_t fanIn)
 	std::random_device rd{};
 	std::mt19937 gen{ rd()};
 
-	std::normal_distribution dis{ 0.f, 2.f / fanIn };
+	std::normal_distribution dis{ 0.f, std::sqrtf( 2.f / fanIn )};
 
 	for (size_t i = 0; i < amount; i++)
 	{
